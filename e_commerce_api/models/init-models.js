ddeec7cdd6be3d79f800f11694edff94e29f621b
wsYYnb3 <danielisraeli033@gmail.com>
2023-09-15 13:43:59 +0200

    VERY IMPORTANT init-models change can break code

--- e_commerce_api/models/init-models.js
+++ e_commerce_api/models/init-models.js
@@ -64,112 +64,305 @@ function initModels(sequelize) {
   var vendor = _vendor(sequelize, DataTypes);
   var wishlistitem = _wishlistitem(sequelize, DataTypes);
 
-  keyword.belongsToMany(product, { as: 'product_id_products', through: productkeyword, foreignKey: "keyword_id", otherKey: "product_id" });
-  product.belongsToMany(keyword, { as: 'keyword_id_keywords', through: productkeyword, foreignKey: "product_id", otherKey: "keyword_id" });
-  customer.belongsTo(address, { as: "shipping_address", foreignKey: "shipping_address_id"});
-  address.hasMany(customer, { as: "customers", foreignKey: "shipping_address_id"});
-  customer.belongsTo(address, { as: "billing_address", foreignKey: "billing_address_id"});
-  address.hasMany(customer, { as: "billing_address_customers", foreignKey: "billing_address_id"});
-  orders.belongsTo(address, { as: "shipping_address", foreignKey: "shipping_address_id"});
-  address.hasMany(orders, { as: "orders", foreignKey: "shipping_address_id"});
-  orders.belongsTo(address, { as: "billing_address", foreignKey: "billing_address_id"});
-  address.hasMany(orders, { as: "billing_address_orders", foreignKey: "billing_address_id"});
-  vendor.belongsTo(address, { as: "address", foreignKey: "address_id"});
-  address.hasMany(vendor, { as: "vendors", foreignKey: "address_id"});
-  product.belongsTo(category, { as: "category", foreignKey: "category_id"});
-  category.hasMany(product, { as: "products", foreignKey: "category_id"});
-  orders.belongsTo(currency, { as: "currency", foreignKey: "currency_id"});
-  currency.hasMany(orders, { as: "orders", foreignKey: "currency_id"});
-  productpriceincurrency.belongsTo(currency, { as: "currency", foreignKey: "currency_id"});
-  currency.hasMany(productpriceincurrency, { as: "productpriceincurrencies", foreignKey: "currency_id"});
-  cartitem.belongsTo(customer, { as: "customer", foreignKey: "customer_id"});
-  customer.hasMany(cartitem, { as: "cartitems", foreignKey: "customer_id"});
-  orders.belongsTo(customer, { as: "customer_customer", foreignKey: "customer_id"});
-  customer.hasMany(orders, { as: "customer_orders", foreignKey: "customer_id"});
-  session.belongsTo(customer, { as: "customer", foreignKey: "customer_id"});
-  customer.hasMany(session, { as: "sessions", foreignKey: "customer_id"});
-  ticket.belongsTo(customer, { as: "customer", foreignKey: "customer_id"});
-  customer.hasMany(ticket, { as: "tickets", foreignKey: "customer_id"});
-  useractivity.belongsTo(customer, { as: "customer", foreignKey: "customer_id"});
-  customer.hasMany(useractivity, { as: "useractivities", foreignKey: "customer_id"});
-  wishlistitem.belongsTo(customer, { as: "customer", foreignKey: "customer_id"});
-  customer.hasMany(wishlistitem, { as: "wishlistitems", foreignKey: "customer_id"});
-  pageimage.belongsTo(image, { as: "image", foreignKey: "image_id"});
-  image.hasMany(pageimage, { as: "pageimages", foreignKey: "image_id"});
-  productcardimage.belongsTo(image, { as: "image", foreignKey: "image_id"});
-  image.hasMany(productcardimage, { as: "productcardimages", foreignKey: "image_id"});
-  productcartimage.belongsTo(image, { as: "image", foreignKey: "image_id"});
-  image.hasMany(productcartimage, { as: "productcartimages", foreignKey: "image_id"});
-  productimage.belongsTo(image, { as: "image", foreignKey: "image_id"});
-  image.hasMany(productimage, { as: "productimages", foreignKey: "image_id"});
-  product.belongsTo(keyword, { as: "keyword", foreignKey: "keyword_id"});
-  keyword.hasMany(product, { as: "products", foreignKey: "keyword_id"});
-  productkeyword.belongsTo(keyword, { as: "keyword", foreignKey: "keyword_id"});
-  keyword.hasMany(productkeyword, { as: "productkeywords", foreignKey: "keyword_id"});
-  translation.belongsTo(language, { as: "language", foreignKey: "language_id"});
-  language.hasMany(translation, { as: "translations", foreignKey: "language_id"});
-  customer.belongsTo(orders, { as: "order", foreignKey: "order_id"});
-  orders.hasOne(customer, { as: "customer", foreignKey: "order_id"});
-  ordersitem.belongsTo(orders, { as: "order", foreignKey: "orders_id"});
-  orders.hasMany(ordersitem, { as: "ordersitems", foreignKey: "orders_id"});
-  pagecontent.belongsTo(page, { as: "page", foreignKey: "page_id"});
-  page.hasMany(pagecontent, { as: "pagecontents", foreignKey: "page_id"});
-  pageimage.belongsTo(page, { as: "page", foreignKey: "page_id"});
-  page.hasMany(pageimage, { as: "pageimages", foreignKey: "page_id"});
-  useractivity.belongsTo(page, { as: "page", foreignKey: "page_id"});
-  page.hasMany(useractivity, { as: "useractivities", foreignKey: "page_id"});
-  cartitem.belongsTo(product, { as: "product", foreignKey: "product_id"});
-  product.hasMany(cartitem, { as: "cartitems", foreignKey: "product_id"});
-  ordersitem.belongsTo(product, { as: "product", foreignKey: "product_id"});
-  product.hasMany(ordersitem, { as: "ordersitems", foreignKey: "product_id"});
-  productcardimage.belongsTo(product, { as: "product", foreignKey: "product_id"});
-  product.hasMany(productcardimage, { as: "productcardimages", foreignKey: "product_id"});
-  productcartimage.belongsTo(product, { as: "product", foreignKey: "product_id"});
-  product.hasMany(productcartimage, { as: "productcartimages", foreignKey: "product_id"});
-  productimage.belongsTo(product, { as: "product", foreignKey: "product_id"});
-  product.hasMany(productimage, { as: "productimages", foreignKey: "product_id"});
-  productkeyword.belongsTo(product, { as: "product", foreignKey: "product_id"});
-  product.hasMany(productkeyword, { as: "productkeywords", foreignKey: "product_id"});
-  productpriceincurrency.belongsTo(product, { as: "product", foreignKey: "product_id"});
-  product.hasMany(productpriceincurrency, { as: "productpriceincurrencies", foreignKey: "product_id"});
-  productspecification.belongsTo(product, { as: "product", foreignKey: "product_id"});
-  product.hasMany(productspecification, { as: "productspecifications", foreignKey: "product_id"});
-  technicalinformation.belongsTo(product, { as: "product", foreignKey: "product_id"});
-  product.hasMany(technicalinformation, { as: "technicalinformations", foreignKey: "product_id"});
-  wishlistitem.belongsTo(product, { as: "product", foreignKey: "product_id"});
-  product.hasMany(wishlistitem, { as: "wishlistitems", foreignKey: "product_id"});
-  useractivity.belongsTo(session, { as: "session", foreignKey: "session_id"});
-  session.hasMany(useractivity, { as: "useractivities", foreignKey: "session_id"});
-  ticket.belongsTo(tickettype, { as: "ticket_type", foreignKey: "ticket_type_id"});
-  tickettype.hasMany(ticket, { as: "tickets", foreignKey: "ticket_type_id"});
-  category.belongsTo(translation, { as: "name_key_translation", foreignKey: "name_key"});
-  translation.hasMany(category, { as: "categories", foreignKey: "name_key"});
-  category.belongsTo(translation, { as: "slug_key_translation", foreignKey: "slug_key"});
-  translation.hasMany(category, { as: "slug_key_categories", foreignKey: "slug_key"});
-  keyword.belongsTo(translation, { as: "keyword_key_translation", foreignKey: "keyword_key"});
-  translation.hasMany(keyword, { as: "keywords", foreignKey: "keyword_key"});
-  page.belongsTo(translation, { as: "name_key_translation", foreignKey: "name_key"});
-  translation.hasMany(page, { as: "pages", foreignKey: "name_key"});
-  page.belongsTo(translation, { as: "slug_key_translation", foreignKey: "slug_key"});
-  translation.hasMany(page, { as: "slug_key_pages", foreignKey: "slug_key"});
-  pagecontent.belongsTo(translation, { as: "content_key_translation", foreignKey: "content_key"});
-  translation.hasMany(pagecontent, { as: "pagecontents", foreignKey: "content_key"});
-  product.belongsTo(translation, { as: "name_key_translation", foreignKey: "name_key"});
-  translation.hasMany(product, { as: "products", foreignKey: "name_key"});
-  product.belongsTo(translation, { as: "slug_key_translation", foreignKey: "slug_key"});
-  translation.hasMany(product, { as: "slug_key_products", foreignKey: "slug_key"});
-  product.belongsTo(translation, { as: "origin_key_translation", foreignKey: "origin_key"});
-  translation.hasMany(product, { as: "origin_key_products", foreignKey: "origin_key"});
-  product.belongsTo(translation, { as: "description_key_translation", foreignKey: "description_key"});
-  translation.hasMany(product, { as: "description_key_products", foreignKey: "description_key"});
-  productspecification.belongsTo(translation, { as: "content_key_translation", foreignKey: "content_key"});
-  translation.hasMany(productspecification, { as: "productspecifications", foreignKey: "content_key"});
-  technicalinformation.belongsTo(translation, { as: "info_key_translation", foreignKey: "info_key"});
-  translation.hasMany(technicalinformation, { as: "technicalinformations", foreignKey: "info_key"});
-  product.belongsTo(unitofmeasure, { as: "unit_of_measure", foreignKey: "unit_of_measure_id"});
-  unitofmeasure.hasMany(product, { as: "products", foreignKey: "unit_of_measure_id"});
-  product.belongsTo(vendor, { as: "vendor", foreignKey: "vendor_id"});
-  vendor.hasMany(product, { as: "products", foreignKey: "vendor_id"});
+  keyword.belongsToMany(product, {
+    as: "product_id_products",
+    through: productkeyword,
+    foreignKey: "keyword_id",
+    otherKey: "product_id",
+  });
+  product.belongsToMany(keyword, {
+    as: "keyword_id_keywords",
+    through: productkeyword,
+    foreignKey: "product_id",
+    otherKey: "keyword_id",
+  });
+  customer.belongsTo(address, {
+    as: "shipping_address",
+    foreignKey: "shipping_address_id",
+  });
+  address.hasMany(customer, {
+    as: "customers",
+    foreignKey: "shipping_address_id",
+  });
+  customer.belongsTo(address, {
+    as: "billing_address",
+    foreignKey: "billing_address_id",
+  });
+  address.hasMany(customer, {
+    as: "billing_address_customers",
+    foreignKey: "billing_address_id",
+  });
+  orders.belongsTo(address, {
+    as: "shipping_address",
+    foreignKey: "shipping_address_id",
+  });
+  address.hasMany(orders, { as: "orders", foreignKey: "shipping_address_id" });
+  orders.belongsTo(address, {
+    as: "billing_address",
+    foreignKey: "billing_address_id",
+  });
+  address.hasMany(orders, {
+    as: "billing_address_orders",
+    foreignKey: "billing_address_id",
+  });
+  vendor.belongsTo(address, { as: "address", foreignKey: "address_id" });
+  address.hasMany(vendor, { as: "vendors", foreignKey: "address_id" });
+  product.belongsTo(category, { as: "category", foreignKey: "category_id" });
+  category.hasMany(product, { as: "products", foreignKey: "category_id" });
+  orders.belongsTo(currency, { as: "currency", foreignKey: "currency_id" });
+  currency.hasMany(orders, { as: "orders", foreignKey: "currency_id" });
+  productpriceincurrency.belongsTo(currency, {
+    as: "currency",
+    foreignKey: "currency_id",
+  });
+  currency.hasMany(productpriceincurrency, {
+    as: "productpriceincurrencies",
+    foreignKey: "currency_id",
+  });
+  cartitem.belongsTo(customer, { as: "customer", foreignKey: "customer_id" });
+  customer.hasMany(cartitem, { as: "cartitems", foreignKey: "customer_id" });
+  orders.belongsTo(customer, {
+    as: "customer_customer",
+    foreignKey: "customer_id",
+  });
+  customer.hasMany(orders, {
+    as: "customer_orders",
+    foreignKey: "customer_id",
+  });
+  session.belongsTo(customer, { as: "customer", foreignKey: "customer_id" });
+  customer.hasMany(session, { as: "sessions", foreignKey: "customer_id" });
+  ticket.belongsTo(customer, { as: "customer", foreignKey: "customer_id" });
+  customer.hasMany(ticket, { as: "tickets", foreignKey: "customer_id" });
+  useractivity.belongsTo(customer, {
+    as: "customer",
+    foreignKey: "customer_id",
+  });
+  customer.hasMany(useractivity, {
+    as: "useractivities",
+    foreignKey: "customer_id",
+  });
+  wishlistitem.belongsTo(customer, {
+    as: "customer",
+    foreignKey: "customer_id",
+  });
+  customer.hasMany(wishlistitem, {
+    as: "wishlistitems",
+    foreignKey: "customer_id",
+  });
+  pageimage.belongsTo(image, { as: "image", foreignKey: "image_id" });
+  image.hasMany(pageimage, { as: "pageimages", foreignKey: "image_id" });
+  productcardimage.belongsTo(image, { as: "image", foreignKey: "image_id" });
+  image.hasMany(productcardimage, {
+    as: "productcardimages",
+    foreignKey: "image_id",
+  });
+  productcartimage.belongsTo(image, { as: "image", foreignKey: "image_id" });
+  image.hasMany(productcartimage, {
+    as: "productcartimages",
+    foreignKey: "image_id",
+  });
+  productimage.belongsTo(image, { as: "image", foreignKey: "image_id" });
+  image.hasMany(productimage, { as: "productimages", foreignKey: "image_id" });
+  product.belongsTo(keyword, { as: "keyword", foreignKey: "keyword_id" });
+  keyword.hasMany(product, { as: "products", foreignKey: "keyword_id" });
+  productkeyword.belongsTo(keyword, {
+    as: "keyword",
+    foreignKey: "keyword_id",
+  });
+  keyword.hasMany(productkeyword, {
+    as: "productkeywords",
+    foreignKey: "keyword_id",
+  });
+  translation.belongsTo(language, {
+    as: "language",
+    foreignKey: "language_id",
+  });
+  language.hasMany(translation, {
+    as: "translations",
+    foreignKey: "language_id",
+  });
+  customer.belongsTo(orders, { as: "order", foreignKey: "order_id" });
+  orders.hasOne(customer, { as: "customer", foreignKey: "order_id" });
+  ordersitem.belongsTo(orders, { as: "order", foreignKey: "orders_id" });
+  orders.hasMany(ordersitem, { as: "ordersitems", foreignKey: "orders_id" });
+  pagecontent.belongsTo(page, { as: "page", foreignKey: "page_id" });
+  page.hasMany(pagecontent, { as: "pagecontents", foreignKey: "page_id" });
+  pageimage.belongsTo(page, { as: "page", foreignKey: "page_id" });
+  page.hasMany(pageimage, { as: "pageimages", foreignKey: "page_id" });
+  useractivity.belongsTo(page, { as: "page", foreignKey: "page_id" });
+  page.hasMany(useractivity, { as: "useractivities", foreignKey: "page_id" });
+  cartitem.belongsTo(product, { as: "product", foreignKey: "product_id" });
+  product.hasMany(cartitem, { as: "cartitems", foreignKey: "product_id" });
+  ordersitem.belongsTo(product, { as: "product", foreignKey: "product_id" });
+  product.hasMany(ordersitem, { as: "ordersitems", foreignKey: "product_id" });
+  productcardimage.belongsTo(product, {
+    as: "product",
+    foreignKey: "product_id",
+  });
+  product.hasMany(productcardimage, {
+    as: "productcardimages",
+    foreignKey: "product_id",
+  });
+  productcartimage.belongsTo(product, {
+    as: "product",
+    foreignKey: "product_id",
+  });
+  product.hasMany(productcartimage, {
+    as: "productcartimages",
+    foreignKey: "product_id",
+  });
+  productimage.belongsTo(product, { as: "product", foreignKey: "product_id" });
+  product.hasMany(productimage, {
+    as: "productimages",
+    foreignKey: "product_id",
+  });
+  productkeyword.belongsTo(product, {
+    as: "product",
+    foreignKey: "product_id",
+  });
+  product.hasMany(productkeyword, {
+    as: "productkeywords",
+    foreignKey: "product_id",
+  });
+  productpriceincurrency.belongsTo(product, {
+    as: "product",
+    foreignKey: "product_id",
+  });
+  product.hasMany(productpriceincurrency, {
+    as: "productpriceincurrencies",
+    foreignKey: "product_id",
+  });
+  productspecification.belongsTo(product, {
+    as: "product",
+    foreignKey: "product_id",
+  });
+  product.hasMany(productspecification, {
+    as: "productspecifications",
+    foreignKey: "product_id",
+  });
+  technicalinformation.belongsTo(product, {
+    as: "product",
+    foreignKey: "product_id",
+  });
+  product.hasMany(technicalinformation, {
+    as: "technicalinformations",
+    foreignKey: "product_id",
+  });
+  wishlistitem.belongsTo(product, { as: "product", foreignKey: "product_id" });
+  product.hasMany(wishlistitem, {
+    as: "wishlistitems",
+    foreignKey: "product_id",
+  });
+  useractivity.belongsTo(session, { as: "session", foreignKey: "session_id" });
+  session.hasMany(useractivity, {
+    as: "useractivities",
+    foreignKey: "session_id",
+  });
+  ticket.belongsTo(tickettype, {
+    as: "ticket_type",
+    foreignKey: "ticket_type_id",
+  });
+  tickettype.hasMany(ticket, { as: "tickets", foreignKey: "ticket_type_id" });
+  category.belongsTo(translation, {
+    as: "name_key_translation",
+    foreignKey: "name_key",
+    targetKey: "translation_key",
+  });
+  translation.hasMany(category, { as: "categories", foreignKey: "name_key" });
+  category.belongsTo(translation, {
+    as: "slug_key_translation",
+    foreignKey: "slug_key",
+    targetKey: "translation_key",
+  });
+  translation.hasMany(category, {
+    as: "slug_key_categories",
+    foreignKey: "slug_key",
+  });
+  keyword.belongsTo(translation, {
+    as: "keyword_key_translation",
+    foreignKey: "keyword_key",
+    targetKey: "translation_key",
+  });
+  translation.hasMany(keyword, { as: "keywords", foreignKey: "keyword_key" });
+  page.belongsTo(translation, {
+    as: "name_key_translation",
+    foreignKey: "name_key",
+  });
+  translation.hasMany(page, { as: "pages", foreignKey: "name_key" });
+  page.belongsTo(translation, {
+    as: "slug_key_translation",
+    foreignKey: "slug_key",
+  });
+  translation.hasMany(page, { as: "slug_key_pages", foreignKey: "slug_key" });
+  pagecontent.belongsTo(translation, {
+    as: "content_key_translation",
+    foreignKey: "content_key",
+  });
+  translation.hasMany(pagecontent, {
+    as: "pagecontents",
+    foreignKey: "content_key",
+  });
+  product.belongsTo(translation, {
+    as: "name_key_translation",
+    foreignKey: "name_key",
+    targetKey: "translation_key",
+  });
+  translation.hasMany(product, { as: "products", foreignKey: "name_key" });
+  product.belongsTo(translation, {
+    as: "slug_key_translation",
+    foreignKey: "slug_key",
+    targetKey: "translation_key",
+  });
+  translation.hasMany(product, {
+    as: "slug_key_products",
+    foreignKey: "slug_key",
+  });
+  product.belongsTo(translation, {
+    as: "origin_key_translation",
+    foreignKey: "origin_key",
+    targetKey: "translation_key",
+  });
+  translation.hasMany(product, {
+    as: "origin_key_products",
+    foreignKey: "origin_key",
+  });
+  product.belongsTo(translation, {
+    as: "description_key_translation",
+    foreignKey: "description_key",
+    targetKey: "translation_key",
+  });
+  translation.hasMany(product, {
+    as: "description_key_products",
+    foreignKey: "description_key",
+  });
+  productspecification.belongsTo(translation, {
+    as: "content_key_translation",
+    foreignKey: "content_key",
+    targetKey: "translation_key",
+  });
+  translation.hasMany(productspecification, {
+    as: "productspecifications",
+    foreignKey: "content_key",
+  });
+  technicalinformation.belongsTo(translation, {
+    as: "info_key_translation",
+    foreignKey: "info_key",
+    targetKey: "translation_key",
+  });
+  translation.hasMany(technicalinformation, {
+    as: "technicalinformations",
+    foreignKey: "info_key",
+  });
+  product.belongsTo(unitofmeasure, {
+    as: "unit_of_measure",
+    foreignKey: "unit_of_measure_id",
+  });
+  unitofmeasure.hasMany(product, {
+    as: "products",
+    foreignKey: "unit_of_measure_id",
+  });
+  product.belongsTo(vendor, { as: "vendor", foreignKey: "vendor_id" });
+  vendor.hasMany(product, { as: "products", foreignKey: "vendor_id" });
 
   return {
     address,
