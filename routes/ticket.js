8002ce6556184bbbbf5dd8d5eb907e7800d36e22
wsYYnb3 <danielisraeli033@gmail.com>
2024-03-23 09:37:28 +0100

    Update ticket.js

--- routes/ticket.js
+++ routes/ticket.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const TicketController = require("../controllers/ticketController");
-const verifyAdmin = require("../middlewares/admin/verifyAdmin");
+const verifyAdminMW = require("../middlewares/admin/verifyAdminMW");
 router.get("/ticket/get/all_ids", TicketController.getAllTicketsID);
-router.get("/ticket/get/all/", verifyAdmin, TicketController.getAllTicket);
+router.get("/ticket/get/all/", verifyAdminMW, TicketController.getAllTicket);
 router.post("/ticket/add/:customerId", TicketController.sendTicket);
 router.put(
   "/ticket/update/:ticketId",
-  verifyAdmin,
+  verifyAdminMW,
   TicketController.updateTicket
 );
 
